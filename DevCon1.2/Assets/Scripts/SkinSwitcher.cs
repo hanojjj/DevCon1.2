1076092079d323917f11c72834c862fe9558bbef
Jonah Cappa <jonahcappaofficial@gmail.com>
2024-10-10 18:22:45 -0400

    Almost finished the different coloured doors

--- DevCon1.2/Assets/Scripts/SkinSwitcher.cs
+++ DevCon1.2/Assets/Scripts/SkinSwitcher.cs
@@ -14,8 +14,16 @@ public class SkinSwitcher : MonoBehaviour
     public Material skin3;
     public Material skin4;
 
-    public int skin;
-    
+    static public int skin;
+
+    public GameObject doorRed;
+    public GameObject doorYellow;
+    public GameObject doorBlue;
+
+    public bool isGreen;
+    public bool isRed;
+    public bool isYellow;
+    public bool isBlue;
 
 
     // Start is called before the first frame update
@@ -23,7 +31,13 @@ public class SkinSwitcher : MonoBehaviour
     {
         skin = 1;
 
+        isGreen = true;
+        isRed = false;
+        isYellow = false;
+        isBlue = false;
+
         player = GameObject.FindGameObjectWithTag("Player");
+        doorBlue = GetComponent<GameObject>();
         render = player.GetComponent<Renderer>();
         render.enabled = true;
     }
@@ -52,19 +66,44 @@ public class SkinSwitcher : MonoBehaviour
         if (skin == 1)
         {
             render.material = skin1;
+
+            isGreen = true;
+            isRed = false;
+            isYellow = false;
+            isBlue = false;
         }
         if (skin == 2)
         {
             render.material = skin2;
+
+            isGreen = false;
+            isRed = false;
+            isYellow = false;
+            isBlue = true;
         }
         if (skin == 3)
         {
             render.material = skin3;
+
+            isGreen = false;
+            isRed = false;
+            isYellow = true;
+            isBlue = false;
         }
         if (skin == 4)
         {
             render.material = skin4;
+
+            isGreen = false;
+            isRed = true;
+            isYellow = false;
+            isBlue = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
     }
 
 }
